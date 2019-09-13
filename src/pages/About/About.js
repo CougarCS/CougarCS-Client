0c117e8e9be1d5709a96b6939d382b207ad93333
Don Aguinaldo <donald.aguinaldo@gmail.com>
2019-09-12 20:14:16 -0500

    Changed layouts of Home, About, Membership page.

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -8,52 +8,48 @@ class About extends React.Component {
   render() {
     return (
       <div className='test'>
-        <Container fluid>
-          <Row>
-            <Col>
-              <div className='about-us'>
-                <h1>About Us</h1>
-                <p>
-                  CougarCS is the largest student run Computer Science
-                  organization at the University of Houston. At CougarCS, our
-                  mission is to smoothly transition our inexperienced members
-                  into young professionals by the end of their degree, and to
-                  provide support and assistance to members who struggle
-                  academically or who need career guidance.
-                </p>
-                <button class="btn" id="getInvolvedBtn">Get Involved</button>
-              </div>
-            </Col>
-            <Col>
-              <img src={gif} alt='undraw svg'></img>
-            </Col>
-          </Row>
-        </Container>
-
-        <Container
-          fluid
-          className='officerCard'
-          style={{ marginBottom: '3em' }}
-        >
-          <h1 style={{ marginTop: '1.8em' }}>Meet the Officers</h1>
-          <Row>
-            <div className='officerImages'>
-              {officers.map((officer) => (
-                <Card key={officer.id}>
-                  <Card.Img variant='top' src={officer.image} />
-                  <br></br>
-                  <Card.Body>
-                    <Card.Title id='officerName'>{officer.name}</Card.Title>
-                    <h5 style={{ textAlign: 'center' }}>{officer.title}</h5>
-                    <p style={{ textAlign: 'center' }}>{officer.email}</p>
-                  </Card.Body>
-                </Card>
-              ))}
+         <div className="about-us-container">
+            {/* networking svg image */}
+            <img src={gif} alt='undraw svg'></img>
+            {/* about us paragraph with call to action button */}
+            <div className='about-us'>
+               <h1>About Us</h1>
+               <p>
+               CougarCS is the largest student run Computer Science
+               organization at the University of Houston. At CougarCS, our
+               mission is to smoothly transition our inexperienced members
+               into young professionals by the end of their degree, and to
+               provide support and assistance to members who struggle
+               academically or who need career guidance.
+               </p>
+               <button class="btn" id="getInvolvedBtn">Get Involved</button>
             </div>
-          </Row>
-        </Container>
+         </div>
+         {/* list of officers */}
+         <Container
+            fluid
+            className='officerCard'
+            style={{ marginBottom: '3em' }}
+         >
+            <h1 style={{ marginTop: '1.8em' }}>Meet the Officers</h1>
+            <Row>
+               <div className='officerImages'>
+               {officers.map((officer) => (
+                  <Card key={officer.id}>
+                     <Card.Img variant='top' src={officer.image} />
+                     <br></br>
+                     <Card.Body>
+                     <Card.Title id='officerName'>{officer.name}</Card.Title>
+                     <h5 style={{ textAlign: 'center' }}>{officer.title}</h5>
+                     <p style={{ textAlign: 'center' }}>{officer.email}</p>
+                     </Card.Body>
+                  </Card>
+               ))}
+               </div>
+            </Row>
+         </Container>
       </div>
-    );
-  }
+      );
+   }
 }
 export default About;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,29 +1,32 @@
 import React from 'react';
-import { Button, ButtonToolbar } from 'react-bootstrap';
+import { Button, ButtonToolbar, Carousel, CarouselItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import './Home.css';
 
 function Main() {
-  return (
-    <div className="ani">
-      <div className="bgDark"> </div>
-      {/* <img className="firstImg" src={bgImg} alt="First slide" /> */}
-      <div className="main-text">
-        <p className="firstP">
-          WE ARE COUGARCS
-          <br></br>
-          WE COMPUTE STUDENT SUCCESS
-        </p>
-        <LinkContainer to="/about">
-          <ButtonToolbar>
-            <Button className="learnMoreBtn" variant="success">
-              Learn More
-            </Button>
-          </ButtonToolbar>
-        </LinkContainer>
+   return (
+      <div className="home">
+         {/* <div className="bgDark"> </div> */}
+         {/* <img className="firstImg" src={bgImg} alt="First slide" /> */}
+         <div className="home-contents">
+            <h1 className="home-header">
+               We are cougarcs
+            </h1>
+            <div className="home-text">
+               <Carousel id="home-carousel">
+                  <CarouselItem>We build</CarouselItem>
+                  <CarouselItem>We develop</CarouselItem>
+                  <CarouselItem>We succeed</CarouselItem>
+               </Carousel>
+            </div>
+            <LinkContainer to="/about">
+               <Button className="learnMoreBtn" variant="primary">
+                  Learn More
+               </Button>
+            </LinkContainer>
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 export default Main;
