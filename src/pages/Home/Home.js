0f4c928126d3f3122580187ba67e6e2b4685a4d9
Siddharth Kundu <sk31395@gmail.com>
2019-09-15 15:31:55 +0530

    fix home page responsive issues

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,21 +6,22 @@ import './Home.css';
 function Main() {
   return (
     <div className="ani">
-      <div className="bgDark"> </div>
-      {/* <img className="firstImg" src={bgImg} alt="First slide" /> */}
-      <div className="main-text">
-        <p className="firstP">
-          WE ARE COUGARCS
-          <br></br>
-          WE COMPUTE STUDENT SUCCESS
-        </p>
-        <LinkContainer to="/about">
-          <ButtonToolbar>
-            <Button className="learnMoreBtn" variant="success">
-              Learn More
-            </Button>
-          </ButtonToolbar>
-        </LinkContainer>
+      <div className="bgDark">
+        {/* <img className="firstImg" src={bgImg} alt="First slide" /> */}
+        <div className="main-text">
+          <p className="firstP">
+            WE ARE COUGARCS
+            <br></br>
+            WE COMPUTE STUDENT SUCCESS
+          </p>
+          <LinkContainer to="/about">
+            <ButtonToolbar>
+              <Button className="learnMoreBtn" variant="success">
+                Learn More
+              </Button>
+            </ButtonToolbar>
+          </LinkContainer>
+        </div>
       </div>
     </div>
   );
