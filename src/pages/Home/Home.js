ace80a545ae69375707f681c98fd8095d5e6100d
Zachary Page <pagezacharyc@gmail.com>
2019-09-15 00:16:33 -0500

    Added new JavaScript library feature to Home Page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,29 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Typed from 'typed.js';
 import { Button, ButtonToolbar } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import './Home.css';
 
-function Main() {
-  return (
-    <div className="ani">
-      <div className="bgDark"> </div>
-      {/* <img className="firstImg" src={bgImg} alt="First slide" /> */}
-      <div className="main-text">
-        <p className="firstP">
-          WE ARE COUGARCS
-          <br></br>
-          WE COMPUTE STUDENT SUCCESS
-        </p>
-        <LinkContainer to="/about">
-          <ButtonToolbar>
-            <Button className="learnMoreBtn" variant="success">
-              Learn More
-            </Button>
-          </ButtonToolbar>
-        </LinkContainer>
-      </div>
-    </div>
-  );
-}
+const animateText = () => (
+  new Typed('#typed', {
+    strings: ["ARE COUGARCS", "COMPUTE <br>STUDENT SUCCESS"],
+    typeSpeed: 70,
+    backSpeed: 75,
+    loop: true,
+  })
+);
 
-export default Main;
+// Class component so you can use `componentDidMount` lifecycle 
+export default class AboutMe extends Component {
+  componentDidMount() {
+    // Will be executed after first `render`
+    animateText();
+  }
+
+  render() {
+    return (
+        <div className="ani">
+          <div className="bgDark"> </div>
+          {/* <img className="firstImg" src={bgImg} alt="First slide" /> */}
+          <div className="main-text">
+            <p className="firstP">
+              WE <span id="typed" />
+            </p>
+            <LinkContainer to="/about">
+              <ButtonToolbar>
+                <Button className="learnMoreBtn" variant="success">
+                  Learn More
+                </Button>
+              </ButtonToolbar>
+            </LinkContainer>
+          </div>
+        </div>
+      );
+    }
+}
