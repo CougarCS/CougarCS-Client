86e0bb2c0231c93c2c69bdf854745b2f69b6719f
Zachary Page <pagezacharyc@gmail.com>
2019-09-26 15:59:28 -0500

    Updated Homepage to a sleeker design, added Typed.JS Library

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,30 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Typed from 'typed.js';
 import { Button, ButtonToolbar } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import './Home.css';
 
-function Main() {
-  return (
-    <div className="ani">
-      <div className="bgDark">
-        {/* <img className="firstImg" src={bgImg} alt="First slide" /> */}
-        <div className="main-text">
-          <p className="firstP">
-            WE ARE COUGARCS
-            <br></br>
-            WE COMPUTE STUDENT SUCCESS
-          </p>
-          <LinkContainer to="/about">
-            <ButtonToolbar>
-              <Button className="learnMoreBtn" variant="success">
-                Learn More
-              </Button>
-            </ButtonToolbar>
-          </LinkContainer>
+const animateText = () => (
+  new Typed('#typed', {
+    strings: ["ARE <span style='color:white;text-shadow: 6px 6px 0px #000,-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;'>COUGARCS</span>", "COMPUTE <br>STUDENT <span style='color:#FFD700'>SUCCESS</span>"],
+    typeSpeed: 70,
+    backSpeed: 75,
+    loop: true,
+  })
+);
+
+// Class component so you can use `componentDidMount` lifecycle 
+export default class AboutMe extends Component {
+  componentDidMount() {
+    // Will be executed after first `render`
+    animateText();
+  }
+
+  render() {
+    return (
+      <div className="ani">
+        <div className="bgDark">
+          <div className="main-text">
+            <p className="firstP">
+              WE <span id="typed" />
+            </p>
+            <LinkContainer to="/about">
+              <ButtonToolbar>
+                <Button className="learnMoreBtn" variant="success">
+                  Learn More
+                </Button>
+              </ButtonToolbar>
+            </LinkContainer>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default Main;
