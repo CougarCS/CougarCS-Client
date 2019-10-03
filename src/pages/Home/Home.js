7af91993f9b01a2b42678b23916fe310af2ef617
Don Aguinaldo <donald.aguinaldo@gmail.com>
2019-10-03 15:08:56 -0500

    merged Home.js from upstream

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,30 +1,44 @@
-import React from 'react';
-import { Button, Carousel, CarouselItem } from 'react-bootstrap';
+
+import React, { Component } from 'react'
+import Typed from 'typed.js';
+import { Button, ButtonToolbar } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import './Home.css';
 
-function Main() {
-  return (
-    <div className="home">
-        <div className="home-contents">
-          <h1 className="home-header">
-              We are cougarcs
-          </h1>
-          <div className="home-text">
-              <Carousel id="home-carousel">
-                <CarouselItem>We build</CarouselItem>
-                <CarouselItem>We develop</CarouselItem>
-                <CarouselItem>We succeed</CarouselItem>
-              </Carousel>
-          </div>
-          <LinkContainer to="/about">
-              <Button className="learnMoreBtn" variant="primary">
-                Learn More
-              </Button>
-          </LinkContainer>
-        </div>
-    </div>
-  );
-}
+const animateText = () => (
+  new Typed('#typed', {
+    strings: ["ARE <span style='color:white;text-shadow: 6px 6px 0px #000,-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;'>COUGARCS</span>", "COMPUTE <br>STUDENT <span style='color:#FFD700'>SUCCESS</span>"],
+    typeSpeed: 70,
+    backSpeed: 75,
+    loop: true,
+  })
+);
 
-export default Main;
+// Class component so you can use `componentDidMount` lifecycle 
+export default class AboutMe extends Component {
+  componentDidMount() {
+    // Will be executed after first `render`
+    animateText();
+  }
+
+  render() {
+    return (
+      <div className="ani">
+        <div className="bgDark">
+          <div className="main-text">
+            <p className="firstP">
+              WE <span id="typed" />
+            </p>
+            <LinkContainer to="/about">
+              <ButtonToolbar>
+                <Button className="learnMoreBtn" variant="success">
+                  Learn More
+                </Button>
+              </ButtonToolbar>
+            </LinkContainer>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
