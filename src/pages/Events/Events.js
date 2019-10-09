d1804b6ab2fa74d7e10609b900841b6928a4c4e8
vyas0189 <vyas01890@outlook.com>
2019-10-09 15:31:56 -0500

    Merged Shubh with master

--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -1,10 +1,10 @@
-import React from "react";
-import "./Event.css";
+import React from 'react';
+import './Event.css';
 
 class Events extends React.Component {
   render() {
     return (
-      <div className="event-container">
+      <div className='event-container'>
         {/* <FullCalendar
           defaultView="dayGridMonth"
           plugins={[dayGridPlugin]}
@@ -13,16 +13,15 @@ class Events extends React.Component {
         /> */}
 
         <iframe
-          title="CougarCS Calendar"
+          title='CougarCS Calendar'
           src={
-            "https://calendar.google.com/calendar/embed?src=aeu0ag4i5a7aag0hkvco4goung%40group.calendar.google.com&ctz=America%2FChicago"
+            'https://calendar.google.com/calendar/embed?src=aeu0ag4i5a7aag0hkvco4goung%40group.calendar.google.com&ctz=America%2FChicago'
           }
           style={{
             border: 0,
             width: '100%',
             height: '100%',
-            frameBorder: 0,
-
+            frameBorder: 0
           }}
         ></iframe>
       </div>
