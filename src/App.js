781b4ca41c80790555ad8a38a5bec0255d77b9fe
vyas0189 <vyas01890@outlook.com>
2019-10-15 10:29:15 -0500

    commented Points route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from "react";
 // import bodyImage from '../assets/'
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import "./App.css";
-import NavBar from "./components/Navbar/Navbar";
-import About from "./pages/About/About";
-import Careerfair from "./pages/CareerFair/Careerfair";
-import Events from "./pages/Events/Events";
-import Home from "./pages/Home/Home";
-import Membership from "./pages/Membership/Membership";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+import NavBar from './components/Navbar/Navbar';
+import About from './pages/About/About';
+import Careerfair from './pages/CareerFair/Careerfair';
+import Events from './pages/Events/Events';
+import Gallery from './pages/Gallery/Gallery';
+import Home from './pages/Home/Home';
+import Membership from './pages/Membership/Membership';
+import Points from './pages/Points/Points';
 const App = () => {
   return (
     <Router>
       <NavBar />
-      <Route path='/' exact component={Home} />
-      <Route path='/about/' component={About} />
-      <Route path='/membership/' component={Membership} />
-      <Route path='/events/' component={Events} />
-      <Route path='/careerfair/' component={Careerfair} />
-      {/* <Route path='/points/' component={Points} /> */}
-    </Router>
+      <Route path="/" exact component={Home} />
+      <Route path="/about/" component={About} />
+      <Route path="/membership/" component={Membership} />
+      <Route path="/events/" component={Events} />
+      <Route path="/careerfair/" component={Careerfair} />
+      <Route path="/points/" component={Points} />
+      <Route path="/gallery/" component={Gallery} />
+    </Router >
   );
 };
 
