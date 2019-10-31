eccee047119ad251b3aaeb9d63d6d348b50a5776
vyas0189 <vyas01890@outlook.com>
2019-10-31 16:19:25 -0500

    Fix responsive size in Membership and officers

--- src/pages/Membership/Membership.js
+++ src/pages/Membership/Membership.js
@@ -1,38 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Card, Col, Container, Row } from 'react-bootstrap';
 import analyticsSVG from '../../assets/analytics.svg';
 import collectingSVG from '../../assets/collecting.svg';
 import gif from '../../assets/member.svg';
 import memorySVG from '../../assets/memory.svg';
 import teachingSVG from '../../assets/teaching.svg';
-class Membership extends React.Component {
+import './Membership.css';
+
+class Membership extends Component {
   render() {
     return (
       <div>
         <Container fluid style={{ padding: '2em' }}>
           <Row>
-            <Col md="6" className="order-md-2">
-              <img src={gif} alt="undraw svg" style={{ width: '100%' }}></img>
+            <Col md='6' className='order-md-2'>
+              <img src={gif} alt='undraw svg' style={{ width: '100%' }}></img>
             </Col>
-            <Col md="6" className="order-md-0">
-              <div className="membership">
+            <Col md='6' className='order-md-0'>
+              <div className='membership'>
                 <h1>Membership</h1>
                 <p>
                   CougarCS offers students exclusive access to a variety of
-                  events and resources aimed at career preparedness.
-                  Throughout every semester, we host dozens of technical
-                  workshops, information sessions, and programming challenges.
-                  Technical workshops and information sessions, often hosted
-                  by Fortune 100 companies, cover a broad spectrum of topics
-                  such as resume critiques, career fair preparation, mock
-                  interviews and much more. Our programming challenges offer
-                  our members a chance to showcase their problem solving
-                  skills in a high energy environment, compete with their
-                  peers, and earn prizes. In addition to these events, we also
-                  provide weekly tutoring and academic support. CougarCS also
-                  has a far reaching network of alumni who are industry
-                  professionals and are always looking to connect with current
-                  members!
+                  events and resources aimed at career preparedness. Throughout
+                  every semester, we host dozens of technical workshops,
+                  information sessions, and programming challenges. Technical
+                  workshops and information sessions, often hosted by Fortune
+                  100 companies, cover a broad spectrum of topics such as resume
+                  critiques, career fair preparation, mock interviews and much
+                  more. Our programming challenges offer our members a chance to
+                  showcase their problem solving skills in a high energy
+                  environment, compete with their peers, and earn prizes. In
+                  addition to these events, we also provide weekly tutoring and
+                  academic support. CougarCS also has a far reaching network of
+                  alumni who are industry professionals and are always looking
+                  to connect with current members!
                 </p>
                 <p>
                   Memberships are priced at{' '}
@@ -40,24 +41,24 @@ class Membership extends React.Component {
                   <strong>$30.00 a semester</strong>. If you are interested in
                   becoming a member there are a few ways you can let us know.
                   Request to become a member on the get involved page and our
-                  Treasurer, Conrad Parker, will be incontact with you
-                  shortly. You can also find an Officer on the 5th floor of
-                  PGH (Philip Guthrie Hoffman Hall) or email{' '}
-                  <a href="mailto:Info@CougarCS.com">Info@CougarCS.com</a>.
+                  Treasurer, Conrad Parker, will be incontact with you shortly.
+                  You can also find an Officer on the 5th floor of PGH (Philip
+                  Guthrie Hoffman Hall) or email{' '}
+                  <a href='mailto:Info@CougarCS.com'>Info@CougarCS.com</a>.
                 </p>
                 <p>
                   Whats that? You want to get more envolved with CougarCS?
                   Interested in joining our webdev team? Please reach out to
                   Vyas Ramankulangara at{' '}
-                  <a href="mailto:Vyas.R@CougarCS.com">Vyas.R@CougarCS.com</a>
-                  . And for helping out at a hackathon or capture the flag
-                  event? Lets get you in contact with our Director of
-                  Hackathons, Athmar Ouda at{' '}
-                  <a href="mailto:Athmar.Ouda@CougarCS.com">
+                  <a href='mailto:Vyas.R@CougarCS.com'>Vyas.R@CougarCS.com</a>.
+                  And for helping out at a hackathon or capture the flag event?
+                  Lets get you in contact with our Director of Hackathons,
+                  Athmar Ouda at{' '}
+                  <a href='mailto:Athmar.Ouda@CougarCS.com'>
                     Athmar.Ouda@CougarCS.com
                   </a>
                   . What about becoming a tutor? For that you can contact{' '}
-                  <a href="mailto:Tutoring@CougarCS.com">
+                  <a href='mailto:Tutoring@CougarCS.com'>
                     Tutoring@cougarcs.com
                   </a>
                   .
@@ -68,7 +69,7 @@ class Membership extends React.Component {
         </Container>
         <Container fluid style={{ textAlign: 'center' }}>
           <h3>What Are The Benefits</h3>
-          <Row style={{ padding: '3em' }} md={12} xs={12}>
+          <Row className='benefits' md={12} xs={12}>
             <div>
               <Row>
                 <Col>
@@ -78,7 +79,7 @@ class Membership extends React.Component {
                         <img
                           src={analyticsSVG}
                           style={{ height: '10em', width: '9em', color: 'red' }}
-                          alt="undraw"
+                          alt='undraw'
                         />
                       </div>
                       <Card.Title style={{ textAlign: 'center' }}>
@@ -96,7 +97,7 @@ class Membership extends React.Component {
                       <div>
                         <img
                           src={collectingSVG}
-                          alt="undraw"
+                          alt='undraw'
                           style={{ height: '10em', width: '9em', color: 'red' }}
                         />
                       </div>
@@ -116,7 +117,7 @@ class Membership extends React.Component {
                       <div>
                         <img
                           src={memorySVG}
-                          alt="undraw"
+                          alt='undraw'
                           style={{ height: '10em', width: '9em', color: 'red' }}
                         />
                       </div>
@@ -131,7 +132,7 @@ class Membership extends React.Component {
                       <div>
                         <img
                           src={teachingSVG}
-                          alt="undraw"
+                          alt='undraw'
                           style={{ height: '10em', width: '9em', color: 'red' }}
                         />
                       </div>
