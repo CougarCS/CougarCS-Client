d4e81a74686305f9b1f615b9c2cd41b56f8fc551
vyas0189 <vyas01890@outlook.com>
2019-11-12 11:22:56 -0600

    Test chagnes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React from 'react';
 // import bodyImage from '../assets/'
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import "./App.css";
-import NavBar from "./components/Navbar/Navbar";
-import About from "./pages/About/About";
-import Careerfair from "./pages/CareerFair/Careerfair";
-import Events from "./pages/Events/Events";
-import Home from "./pages/Home/Home";
-import Membership from "./pages/Membership/Membership";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+import NavBar from './components/Navbar/Navbar';
+import About from './pages/About/About';
+import Careerfair from './pages/CareerFair/Careerfair';
+import Events from './pages/Events/Events';
+import Home from './pages/Home/Home';
+import Membership from './pages/Membership/Membership';
+import Test from './pages/Test/Test';
 const App = () => {
   return (
     <Router>
@@ -18,6 +19,7 @@ const App = () => {
       <Route path='/events/' component={Events} />
       <Route path='/careerfair/' component={Careerfair} />
       {/* <Route path='/points/' component={Points} /> */}
+      <Route path='/test/' component={Test} />
     </Router>
   );
 };
