a4153fa9cf960a9c5e77ca4a3174ac579e5ded57
Vyas Ramankulangara <vyas01890@outlook.com>
2019-11-26 10:42:18 -0600

    Revert "Vyas"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactGA from "react-ga";
+// import bodyImage from '../assets/'
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/Navbar/Navbar";
@@ -8,7 +9,6 @@ import Careerfair from "./pages/CareerFair/Careerfair";
 import Events from "./pages/Events/Events";
 import Home from "./pages/Home/Home";
 import Membership from "./pages/Membership/Membership";
-import News from "./pages/News/News";
 
 function initializeReactGA() {
   ReactGA.initialize("UA-152061658-1");
@@ -22,9 +22,8 @@ const App = () => {
       <Route path="/" exact component={Home} />
       <Route path="/about/" component={About} />
       <Route path="/membership/" component={Membership} />
-      <Route path="/calendar/" component={Events} />
+      <Route path="/events/" component={Events} />
       <Route path="/careerfair/" component={Careerfair} />
-      <Route path="/news/" component={News} />
       {/* <Route path='/points/' component={Points} /> */}
     </Router>
   );
