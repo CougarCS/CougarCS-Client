66fb71cb52f7ba7667b34beb142b0588292665ac
HaiVu <hai_dinh_vu@yahoo.com>
2019-12-25 17:18:13 -0600

    Updating 12/25/19

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import React from "react";
-// import bodyImage from '../assets/'
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import ReactGA from "react-ga";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/Navbar/Navbar";
 import About from "./pages/About/About";
 import Careerfair from "./pages/CareerFair/Careerfair";
+import ContactUs from "./pages/ContactUs/ContactUs";
 import Events from "./pages/Events/Events";
 import Home from "./pages/Home/Home";
 import Membership from "./pages/Membership/Membership";
-import Tutoring from "./pages/Tutoring/Tutoring";
+import NotFound from "./pages/NotFound/NotFound";
 
-function App() {
+function initializeReactGA() {
+  ReactGA.initialize("UA-152061658-1");
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
+
+const App = () => {
+  initializeReactGA();
   return (
     <Router>
       <NavBar />
-      <Route path="/" exact component={Home} />
-      <Route path="/about/" component={About} />
-      <Route path="/membership/" component={Membership} />
-      <Route path="/events/" component={Events} />
-      <Route path="/careerfair/" component={Careerfair} />
-      <Route path="/tutoring" component={Tutoring} />
+      <Switch>
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/about/" component={About} />
+        <Route path="/membership/" component={Membership} />
+        <Route path="/calendar/" component={Events} />
+        <Route path="/careerfair/" component={Careerfair} />
+        {/* <Route path="/points/" component={Points} /> */}
+        <Route path="/contactus/" component={ContactUs} />
+        <Route component={NotFound} />
+      </Switch>
     </Router>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Contact/Contact.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./Contact.css";
+
+class Contact extends Component {
+  state = {};
+  render() {
+    return <h1>"hello"</h1>;
+  }
+}
+
+export default Contact;
