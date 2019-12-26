6e718b4ad4a7c0136d49545830acb6ddeff7d6cb
vyas0189 <vyas01890@outlook.com>
2019-12-25 19:26:09 -0600

    removed 404

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactGA from "react-ga";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/Navbar/Navbar";
 import About from "./pages/About/About";
@@ -9,7 +9,6 @@ import ContactUs from './pages/ContactUs/ContactUs';
 import Events from "./pages/Events/Events";
 import Home from "./pages/Home/Home";
 import Membership from "./pages/Membership/Membership";
-import NotFound from "./pages/NotFound/NotFound";
 
 function initializeReactGA() {
   ReactGA.initialize("UA-152061658-1");
@@ -21,16 +20,16 @@ const App = () => {
   return (
     <Router>
       <NavBar />
-      <Switch>
-        <Route path="/" exact={true} component={Home} />
-        <Route path="/about/" component={About} />
-        <Route path="/membership/" component={Membership} />
-        <Route path="/calendar/" component={Events} />
-        <Route path="/careerfair/" component={Careerfair} />
-        {/* <Route path="/points/" component={Points} /> */}
-        <Route path="/contactus/" component={ContactUs} />
-        <Route component={NotFound} />
-      </Switch>
+      {/* <Switch> */}
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about/" component={About} />
+      <Route path="/membership/" component={Membership} />
+      <Route path="/calendar/" component={Events} />
+      <Route path="/careerfair/" component={Careerfair} />
+      {/* <Route path="/points/" component={Points} /> */}
+      <Route path="/contactus/" component={ContactUs} />
+      {/* <Route component={NotFound} /> */}
+      {/* </Switch> */}
     </Router >
   );
 };
