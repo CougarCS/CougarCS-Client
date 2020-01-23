8ece4dc5706cfaa5d677db9f492e89aec48617bb
Vyas Ramankulangara <vyas01890@outlook.com>
2020-01-23 10:20:30 -0600

    fixed officer pics and removed info about atmar

--- src/officers.js
+++ src/officers.js
@@ -1,15 +1,10 @@
 import conrad from './assets/conrad_p-2.jpeg';
 import mare from './assets/fs_mare.jpg';
-import harrison from './assets/harrison.png';
 import ibrahim from './assets/ibrahim_k.jpeg';
-import jacob from './assets/jake_m.jpeg';
 import jesus from './assets/jesus.png';
-import madeleine from './assets/madeleine_w.jpg';
 import naveed from './assets/naveed.JPG';
-import nilanjan from './assets/nilanjan.jpeg';
-import rizwan from './assets/rizwan.png';
+import user from './assets/users-01.png';
 import vyas from './assets/vyas_r-1.jpeg';
-import zach from './assets/zach.jpeg';
 
 const officers = [
   {
@@ -22,11 +17,19 @@ const officers = [
   },
   {
     id: 2,
-    name: 'Harrison Hayes',
-    image: harrison,
+    name: 'Naveed Panjwani',
+    image: naveed,
     email: 'harrison.hayes@cougarcs.com',
-    linkedin: 'https://www.linkedin.com/in/harrisonhayes/',
+    linkedin: 'https://www.linkedin.com/in/naveed-panjwani/',
     title: 'Vice President',
+  },
+  {
+    id: 3,
+    name: 'Mariana Villalobos',
+    image: mare,
+    linkedin: 'https://www.linkedin.com/in/mariana07/',
+    email: 'mariana.villalobos@cougarcs.com',
+    title: 'Secretary',
   },
   {
     id: 4,
@@ -34,18 +37,10 @@ const officers = [
     image: conrad,
     linkedin: 'https://www.linkedin.com/in/conrad-parker/',
     email: 'conrad.parker@cougarcs.com',
-    title: 'Treasure',
+    title: 'Treasurer',
   },
   {
-    id: 3,
-    name: 'Naveed Panjwani',
-    image: naveed,
-    linkedin: 'https://www.linkedin.com/in/naveed-panjwani/',
-    email: 'n.panjwani@cougarcs.com',
-    title: 'Secretary',
-  },
-  {
-    id: 6,
+    id: 5,
     name: 'Vyas Ramankulangara',
     image: vyas,
     linkedin: 'https://www.linkedin.com/in/vyas0189/',
@@ -53,60 +48,44 @@ const officers = [
     title: 'Webmaster',
   },
   {
-    id: 5,
-    name: 'Jacob Medel',
-    image: jacob,
-    linkedin: 'https://www.linkedin.com/in/jacobmedel/',
-    email: 'jacob.medel@cougarcs.com',
-    title: 'Director of Communications',
-  },
-  {
-    id: 7,
+    id: 6,
     name: 'Jesus Del Riego',
     image: jesus,
     linkedin: 'https://www.linkedin.com/in/jesusdelr/',
     email: 'jesus.delr@cougarcs.com',
+    title: 'Director of Communications',
+  },
+  {
+    id: 7,
+    name: 'Chantha Mak',
+    image: user,
+    linkedin: 'https://www.linkedin.com/in/chantha-mak-830706177/',
+    email: 'chantha.mak@cougarcs.com',
     title: 'Director of Operations',
   },
   {
     id: 8,
-    name: 'Nilanjan Chowdhury',
-    image: nilanjan,
-    linkedin: 'https://www.linkedin.com/in/nilanjanc-cs21/',
-    email: 'nilanjan.chowdhury@cougarcs.com',
-    title: 'Designer',
+    name: 'Mubashir Khan',
+    image: user,
+    linkedin: 'https://www.linkedin.com/in/mubashir-khan-20808917a/',
+    email: 'mubashir.khan@cougarcs.com',
+    title: 'Co-Director Of Activities',
   },
   {
     id: 9,
-    name: 'Madeleine Wilson',
-    image: madeleine,
-    linkedin: 'https://www.linkedin.com/in/madeleine-faye-wilson/',
-    email: 'mfwilson@cougarcs.com',
-    title: 'Director of Tutoring',
+    name: 'Jerry Lopez',
+    image: user,
+    linkedin: 'https://www.linkedin.com/in/gerardo-lopez-374853181/',
+    email: 'gerardo.lopez@cougarcs.com',
+    title: 'Co-Director Of Activities',
   },
   {
     id: 10,
-    name: 'Rizwan Khan',
-    image: rizwan,
-    linkedin: 'https://www.linkedin.com/in/rkhan01/',
-    email: 'rizwan.khan@cougarcs.com',
-    title: 'Director of Activities',
-  },
-  {
-    id: 11,
-    name: 'Mariana Villalobos',
-    image: mare,
-    linkedin: 'https://www.linkedin.com/in/mariana07/',
-    email: 'mariana.villalobos@cougarcs.com',
-    title: 'Historian',
-  },
-  {
-    id: 12,
-    name: 'Zachery Page',
-    image: zach,
-    linkedin: 'https://www.linkedin.com/in/pagezacharyc/',
-    email: 'zach.page@cougarcs.com',
-    title: 'Historian',
+    name: 'Fahad Siddiqi',
+    image: user,
+    linkedin: 'https://www.linkedin.com/in/fasiddiqi/',
+    email: 'fahad.siddiqi@cougarcs.com',
+    title: 'Director of Tutoring',
   },
 ];
 
