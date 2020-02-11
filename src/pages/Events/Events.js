dd9d7699bf0c5d93ffccae5c81e306e51999d43f
Vyas Ramankulangara <vyas01890@outlook.com>
2020-02-11 12:56:35 -0600

    added new cal

--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -1,54 +1,77 @@
-// import axios from 'axios';
-// import moment from 'moment';
-import React from 'react';
-// import { Calendar, momentLocalizer } from 'react-big-calendar';
+import axios from 'axios';
+import moment from 'moment';
+import React, { useEffect, useState } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import Loading from '../../components/Loading/Loading';
 import './Event.css';
 
-// const localizer = momentLocalizer(moment)
-// const url = `https://www.googleapis.com/calendar/v3/calendars/aeu0ag4i5a7aag0hkvco4goung@group.calendar.google.com/events?key=AIzaSyBZcSmQlmAJa0h0prJhOLUe1_Ssk57iHkE`;
+const localizer = momentLocalizer(moment)
+const url = `https://cougarcs-backend.herokuapp.com/api/events`;
 
 const Events = () => {
 
-  // const [events, setEvents] = useState([]);
-  // useEffect(() => {
-  //   axios.get(url)
-  //     .then(resp => {
-  //       const events = []
-  //       resp.data.items.map((event) => {
-  //         if (event.start === undefined) {
-  //           return;
-  //         }
-  //         events.push({
-  //           start: event.start.date || event.start.dateTime,
-  //           end: event.end.date || event.end.dateTime,
-  //           title: event.summary,
-  //         })
-  //       })
-  //       setEvents(events);
-  //     }).catch((error) => {
-  //       console.log(error);
-  //     })
-  // }, [])
+  const [events, setEvents] = useState([]);
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    axios.get(url)
+      .then(resp => {
+        const events = []
+        resp.data.futureEvents.map((event) => {
+          return events.push({
+            start: event.start.date,
+            end: event.end.date,
+            title: event.summary,
+            desc: event.description ? event.description : 'TBD'
+          })
+        })
+
+        resp.data.pastEvents.map((event) => {
+          return events.push({
+            start: event.start.date,
+            end: event.end.date,
+            title: event.summary,
+            desc: event.description ? event.description : 'TBD'
+          })
+        })
+        setEvents(events);
+        setLoading(false);
+      }).catch((error) => {
+        console.log(error);
+      })
+  }, [])
   return (
-    < div className='event-container' >
-      {/* <Calendar
-        localizer={localizer}
-        events={events}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: "100%" }}
-      /> */}
-      <iframe
+    <div>
+      {loading ? <Loading className="loader" /> :
+        < div className='event-container' >
+          <Calendar
+            localizer={localizer}
+            events={events}
+            startAccessor="start"
+            endAccessor="end"
+            style={{ height: "100%" }}
+            views={{
+              month: true,
+              agenda: true
+            }}
+            popup={true}
+            drilldownView="agenda"
+            popupOffset={{ x: 30, y: 20 }}
+          />
+
+          {/* <iframe
         title='CougarCS Calendar'
-        src={'https://www.google.com/calendar/embed?showTitle=0&showCalendars=0&mode=AGENDA&wkst=1&bgcolor=%23FFFFFF&src=aeu0ag4i5a7aag0hkvco4goung%40group.calendar.google.com&color=%23711616&ctz=America%2FChicago'}
+        src={'https://www.google.com/calendar/embed?showTitle=0&showCalendars=0&mode=MONTH&wkst=1&bgcolor=%23FFFFFF&src=aeu0ag4i5a7aag0hkvco4goung%40group.calendar.google.com&color=%23711616&ctz=America%2FChicago'}
         style={{
           border: 0,
           width: '100%',
           height: '100%',
           frameBorder: 0
         }}
-      ></iframe>
-    </div >
+      ></iframe> */}
+        </div >
+      }
+    </div>
   )
 }
 export default Events;
