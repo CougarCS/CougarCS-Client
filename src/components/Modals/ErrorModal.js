f6dec8a50d3237ced81a3551b4b4a75f7700385e
Shubh Sheth <shubhsheth8@gmail.com>
2020-09-10 20:01:50 -0500

    Added Success and Error Modals

---
+++ src/components/Modals/ErrorModal.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Modal, Image} from 'react-bootstrap';
+import './Modal.css';
+import warn from '../../assets/warn.png'
+
+export const ErrorModal = (props) => {
+
+	return (
+		<Modal show={props.show} onHide={props.handleClose}>
+			<Modal.Header closeButton>
+			</Modal.Header>
+			<Modal.Body>
+				<Image className="warning-image" src={warn} />
+				<Modal.Title>Error!</Modal.Title>
+				<p>
+					Something went wrong. Please try again or contact <a href="mailto:treasurer@cougarcs.com">treasurer@cougarcs.com</a> to pay with an alternative method.
+				</p>
+			</Modal.Body>
+		</Modal>
+	)
+}
+export default ErrorModal
