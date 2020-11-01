9c44c2ada460f84926f0c5d09fe1c199aae4f413
Vyas Ramankulangara <vyas01890@outlook.com>
2020-11-01 15:46:05 -0600

    disable dark mode

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -34,7 +34,7 @@ const NavBar = ({ themeToggler }) => (
 					<Nav.Link>Contact Us</Nav.Link>
 				</LinkContainer>
 				<Nav.Link href='https://uhcode.red/'>CodeRED</Nav.Link>
-				<DarkMode />
+				{/* <DarkMode /> */}
 			</Nav>
 		</Navbar.Collapse>
 	</Navbar>
