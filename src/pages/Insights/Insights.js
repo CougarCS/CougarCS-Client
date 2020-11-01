fcbe5bd557cfa985526068e04875ccbeda2dd50a
Richard Paredes <52340573+richard-paredes@users.noreply.github.com>
2020-11-01 17:00:28 -0600

    added basic layout

--- src/pages/Insights/Insights.js
+++ src/pages/Insights/Insights.js
@@ -1,47 +1,50 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row, Jumbotron, Button, Card } from 'react-bootstrap';
 import gif from '../../assets/about.svg';
-import Officer from '../../components/Officers/Officer';
-import officers from '../../officers';
 import './Insights.css';
 
 const Insights = () => {
-		return (
-			<div className='test'>
-				<Container fluid>
-					<Row className='rowHeight'>
-						<Col md='6' className='order-md-2'>
-							<img src={gif} alt='undraw svg' className='img-fluid'></img>
-						</Col>
-						<Col md='6' className='order-md-0'>
-							<div className='about-us'>
-								<h1>About Us</h1>
-								<p>
-									CougarCS is the largest student run Computer Science
-									organization at the University of Houston. At CougarCS, our
-									mission is to smoothly transition our inexperienced members
-									into young professionals by the end of their degree, and to
-									provide support and assistance to members who struggle
-									academically or who need career guidance.
-								</p>
-							</div>
-						</Col>
-					</Row>
-				</Container>
 
-				<Container className='officerCard' fluid>
-					<h1 style={{ marginTop: '1.8em' }}>Meet the Officers</h1>
-					<Row className='cnt'>
-						<div className='officerImages'>
-							{officers.map((officer) =>
-								officer.show ? (
-									<Officer officer={officer} key={officer.id} />
-								) : null
-							)}
+		return (
+			<Container fluid className="main">
+				<Jumbotron fluid className="hero">
+					<h1>Cougar Insights</h1>
+				</Jumbotron>
+				<Container className="secondary container">
+					<Container className="job-content">
+						<div className="search">
+							<div className="search-input">
+								<input />
+								<button>
+									Search
+								</button>
+							</div>
+							<h6>
+								1,352 jobs found
+							</h6>
 						</div>
-					</Row>
+						<div className="jobs">
+							<Row>
+								<Col>
+									<Card className="job-card">
+										Hello
+									</Card>
+								</Col>
+								<Col>
+									<Card className="job-card">
+										Hello
+									</Card>
+								</Col>
+								<Col>
+									<Card className="job-card">
+										Hello
+									</Card>
+								</Col>
+							</Row>
+						</div>
+					</Container>
 				</Container>
-			</div>
+			</Container>
 		);
 }
 export default Insights;
---
+++ src/pages/Insights/jobs.js
@@ -0,0 +1,14 @@
+export const jobs = [
+    {
+        company: {
+            name: "",
+            location: "",
+            imageUrl: ""
+        },
+        title: "",
+        skills: [""],
+        salary: "",
+        description: "",
+        applicationLink: ""
+    }
+]
