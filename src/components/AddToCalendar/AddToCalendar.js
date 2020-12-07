d554a401d72dab62d30598a725f1e2ac66d3eeb3
Vyas Ramankulangara <vyas01890@outlook.com>
2020-12-07 10:29:25 -0600

    added ics file support

--- src/components/AddToCalendar/AddToCalendar.js
+++ src/components/AddToCalendar/AddToCalendar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Dropdown } from 'react-bootstrap';
 import moment from 'moment';
-import { google, outlook } from "calendar-link";
+import { google, outlook, ics } from "calendar-link";
 
 const AddToCalendar = ({ event }) => {
     const { startDate, endDate } = event;
@@ -13,7 +13,9 @@ const AddToCalendar = ({ event }) => {
                 rel='noopener nofollow'>Google</Dropdown.Item>
             <Dropdown.Item href={outlook({ ...event, start: moment(startDate).format(), end: moment(endDate).format() }).replace("&rru=addevent", "")} target='_blank'
                 rel='noopener nofollow'>Outlook.com</Dropdown.Item>
+            <Dropdown.Item href={ics({ ...event, start: moment(startDate).format(), end: moment(endDate).format() })}>Others</Dropdown.Item>
         </Dropdown.Menu >
+
     )
 }
 
