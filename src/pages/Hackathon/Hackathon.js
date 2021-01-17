48a0bdebe92a81aef27d3da519a440ee188fba31
Shubh Sheth <shubhsheth8@gmail.com>
2021-01-17 14:45:14 -0600

    Hackathon - Convert to functional component

--- src/pages/Hackathon/Hackathon.js
+++ src/pages/Hackathon/Hackathon.js
@@ -1,38 +1,37 @@
-import React from 'react';
-import { Container, Row } from 'react-bootstrap';
+import React, {useState} from 'react';
+import { Container } from 'react-bootstrap';
 import hackathons from '../../hackathonlists';
 import HackathonComp from '../../components/Hackathon/hackathonsComp';
 import './Hackathon.css';
 
-class Hackathon extends React.Component {
-    state={
-        currTime : new Date().toISOString().split("T")[0]
-    }
-    render(){
-        return(
-            <Container className="hackathonCard" fluid>
-                <h1>Upcoming Hackthons</h1>
-                    <Row className="each">
-                        <div className="hackathonLogos">
-                            {
-                                hackathons.sort((a, b) => (a.year.concat(a.startDate) > b.year.concat(b.startDate)) ? 1:-1).map((hackathonlist) =>
-                                    (hackathonlist.year.concat(hackathonlist.endDate) > this.state.currTime) ? (
-                                            <HackathonComp hackathonlist = {hackathonlist} key = {hackathonlist.id}/> 
-                                    ) : null     
-                            )}
-                        </div>
-                        <h1>Past Hackathons</h1>
-                        <div className="hackathonLogos past">
-                            {
-                                 hackathons.sort((a, b) => (a.year.concat(a.startDate) < b.year.concat(b.startDate)) ? 1:-1).map((hackathonlist) =>
-                                 (hackathonlist.year.concat(hackathonlist.endDate) < this.state.currTime) ? (
-                                         <HackathonComp hackathonlist = {hackathonlist} key = {hackathonlist.id}/> 
-                                 ) : null
-                            )}
-                        </div>
-                    </Row>
-            </Container>
-        )
-    }
+const Hackathon = () => {
+
+    const [currTime, updateCurrTime] = useState(new Date().toISOString().split("T")[0]);
+
+    return (
+        <Container className="hackathonCard" fluid>
+            <h1>Upcoming Hackthons</h1>
+            <div className="hackathonLogos">
+                {
+                hackathons.sort((a, b) => (a.year.concat(a.startDate) > b.year.concat(b.startDate)) ? 1:-1).map((hackathonlist) =>
+                    (hackathonlist.year.concat(hackathonlist.endDate) > currTime) ? (
+                        <HackathonComp hackathonlist = {hackathonlist} key = {hackathonlist.id}/> 
+                    ) : null     
+                )
+                }
+            </div>
+            <h1>Past Hackathons</h1>
+            <div className="hackathonLogos past">
+                {
+                hackathons.sort((a, b) => (a.year.concat(a.startDate) < b.year.concat(b.startDate)) ? 1:-1).map((hackathonlist) =>
+                    (hackathonlist.year.concat(hackathonlist.endDate) < currTime) ? (
+                        <HackathonComp hackathonlist = {hackathonlist} key = {hackathonlist.id}/> 
+                    ) : null
+                )
+                }
+            </div>
+        </Container>
+    )
 }
+
 export default Hackathon;
