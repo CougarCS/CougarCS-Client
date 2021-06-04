1949418e28f612636ef2a5469ebb4e9cecfe8e16
Kevin <floreskevin51398@gmail.com>
2021-06-04 10:04:37 -0500

    Updates Officer Images and positions

--- src/data/officers.js
+++ src/data/officers.js
@@ -1,14 +1,16 @@
 import Dr_Yun from '../assets/officers/Dr_Yun.jpg';
 
-import Mariana from '../assets/officers/Mariana.jpg';
+
 import Nykolas from '../assets/officers/Nykolas.jpg';
-import Chantha from '../assets/officers/Chantha.jpg';
-import Sevilay from '../assets/officers/Sevilay.jpg';
 import Shubh from '../assets/officers/Shubh.jpg';
-import Shubha from '../assets/officers/Shubha.jpg';
-import Tooba from '../assets/officers/Tooba.jpg';
-import Neriyah from '../assets/officers/Neriyah.jpg';
-import Adil from '../assets/officers/Adil.jpg';
+import Zuha from '../assets/officers/Zuha.jpg';
+import Kevin from '../assets/officers/Kevin.jpg';
+import Ahmed from '../assets/officers/Ahmed.jpg';
+import Christian from '../assets/officers/Christian.jpg';
+import Erick from '../assets/officers/Erick.jpg';
+import Armando from '../assets/officers/Armando.jpg';
+import Matthew from '../assets/officers/Matthew.jpg';
+
 
 const officers = [
 	{
@@ -23,9 +25,9 @@ const officers = [
 	},
 	{
 		id: 1,
-		name: 'Mariana Villalobos',
-		image: Mariana,
-		linkedin: 'https://www.linkedin.com/in/mariana07/',
+		name: 'Nykolas Farhangi',
+		image: Nykolas,
+		linkedin: 'https://www.linkedin.com/in/nykolasfarhangi/',
 		email: 'president@cougarcs.com',
 		title: 'President',
 		open: false,
@@ -33,9 +35,9 @@ const officers = [
 	},
 	{
 		id: 2,
-		name: 'Nykolas Farhangi',
-		image: Nykolas,
-		linkedin: 'https://www.linkedin.com/in/nykolasfarhangi/',
+		name: 'Shubh Sheth',
+		image: Shubh,
+		linkedin: 'https://www.linkedin.com/in/sheth-shubh/',
 		email: 'vicepresident@cougarcs.com',
 		title: 'Vice President',
 		open: false,
@@ -43,9 +45,9 @@ const officers = [
 	},
 	{
 		id: 3,
-		name: 'Chantha Mak',
-		image: Chantha,
-		linkedin: 'https://www.linkedin.com/in/chantha-mak-830706177/',
+		name: 'Syeda Zuha Beyabani',
+		image: Zuha,
+		linkedin: 'https://www.linkedin.com/in/syedazuhabeyabani/',
 		email: 'treasurer@cougarcs.com',
 		title: 'Treasurer',
 		open: false,
@@ -53,9 +55,9 @@ const officers = [
 	},
 	{
 		id: 4,
-		name: 'Shubh Sheth',
-		image: Shubh,
-		linkedin: 'https://www.linkedin.com/in/sheth-shubh/',
+		name: 'Kevin Flores',
+		image: Kevin,
+		linkedin: 'https://www.linkedin.com/in/flores-kevin/',
 		email: 'webmaster@cougarcs.com',
 		title: 'Webmaster',
 		open: false,
@@ -63,9 +65,9 @@ const officers = [
 	},
 	{
 		id: 5,
-		name: 'Sevilay Ozturk',
-		image: Sevilay,
-		linkedin: 'https://www.linkedin.com/in/sevilay-ozturkk/',
+		name: 'Ahmed Qureshi',
+		image: Ahmed,
+		linkedin: 'https://www.linkedin.com/in/aoquresh/',
 		email: 'communications@cougarcs.com',
 		title: 'Director of Communications',
 		open: false,
@@ -73,9 +75,9 @@ const officers = [
 	},
 	{
 		id: 6,
-		name: 'Shubha Joshi',
-		image: Shubha,
-		linkedin: 'https://www.linkedin.com/in/shubha-joshi-03441a194/',
+		name: 'Christian Ayala',
+		image: Christian,
+		linkedin: 'https://www.linkedin.com/in/christian-ayala--/',
 		email: 'historian@cougarcs.com',
 		title: 'Historian',
 		open: false,
@@ -83,9 +85,9 @@ const officers = [
 	},
 	{
 		id: 7,
-		name: 'Tooba Hashmi',
-		image: Tooba,
-		linkedin: 'https://www.linkedin.com/in/tooba-hashmi-540a73156/',
+		name: 'Erick-Khai Pham',
+		image: Erick,
+		linkedin: 'https://www.linkedin.com/in/erickkhaipham/',
 		email: 'activities@cougarcs.com',
 		title: 'Co-Director of Activities',
 		open: false,
@@ -93,9 +95,9 @@ const officers = [
 	},
 	{
 		id: 8,
-		name: 'Neriyah Butler',
-		image: Neriyah,
-		linkedin: 'https://www.linkedin.com/in/neriyah-butler/',
+		name: 'Armando Morales',
+		image: Armando,
+		linkedin: 'https://www.linkedin.com/in/armando-morales-454431167/',
 		email: 'activities@cougarcs.com',
 		title: 'Co-Director of Activities',
 		open: false,
@@ -103,9 +105,9 @@ const officers = [
 	},
 	{
 		id: 9,
-		name: 'Adil Iqbal',
-		image: Adil,
-		linkedin: 'https://www.linkedin.com/in/aiqbal-cs/',
+		name: 'Matthew Philip',
+		image: Matthew,
+		linkedin: 'https://www.linkedin.com/in/matthew-philip/',
 		email: 'tutoring@cougarcs.com',
 		title: 'Director of Tutoring',
 		open: false,
