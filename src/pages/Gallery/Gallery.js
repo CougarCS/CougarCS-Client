295e6b59574e803920957832dce0f877ad1c628a
Vyas Ramankulangara <vyas01890@outlook.com>
2021-07-04 15:54:49 -0500

    Fix 403: Master to PROD (#209) (#213)
    * fix 403 error
    
    Co-authored-by: shubhsheth <shubhsheth8@gmail.com>
    Co-authored-by: Kevin Flores <31356159+KevFlo@users.noreply.github.com>

--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -21,19 +21,18 @@ const Gallery = () => {
 			</Container>
 			<Container fluid className='contained'>
 				<div className='events'>
-					{events.map(
-						(event) =>
-							event.show && (
-								<Card className='gallery-card' key={event.id}>
-									<Card.Img src={event.image} />
-									<Card.Body>
-										<Card.Title>{event.name}</Card.Title>
-										<Link to={`/gallery/${event.link}`} className='event-link'>
-											<p id='view-event'>View Event</p>
-										</Link>
-									</Card.Body>
-								</Card>
-							)
+					{events.map((event) =>
+						event.show ? (
+							<Card className='gallery-card' key={event.id}>
+								<Card.Img src={event.image} />
+								<Card.Body>
+									<Card.Title>{event.name}</Card.Title>
+									<Link to={`/gallery/${event.link}`} className='event-link'>
+										<p id='view-event'>View Event</p>
+									</Link>
+								</Card.Body>
+							</Card>
+						) : null
 					)}
 				</div>
 			</Container>
