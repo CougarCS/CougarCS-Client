9451b4f72db544a1d158e073877235883883b744
Vyas Ramankulangara <vyas01890@outlook.com>
2021-08-23 19:53:40 -0500

    rm logrocket

--- src/utils/reactGA.js
+++ src/utils/reactGA.js
@@ -1,11 +1,9 @@
-import LogRocket from 'logrocket';
 import ReactGA from 'react-ga';
 
 const snap = navigator.userAgent !== 'ReactSnap';
 const isProd = process.env.REACT_APP_NODE_ENV === 'PROD';
 
 export const initialize = () => {
-	LogRocket.init('mzjohu/cougarcs');
 	if (snap && isProd) {
 		ReactGA.initialize('UA-155177558-1');
 		ReactGA.ga('set', 'transport', 'beacon');
