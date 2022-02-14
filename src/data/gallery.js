85d64a5176b2a2fcb90f96383ba7203c9de05a67
Ali Zain Charolia <63433077+alizain-c@users.noreply.github.com>
2022-02-14 00:55:47 -0600

    Update gallery.js with Welcome back social
    Welcome back social tab and pics added.

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -26,8 +26,27 @@ import CatsBack21_2 from '../assets/gallery/catsback2.webp';
 import BYOK1 from '../assets/gallery/byok1.webp';
 import BYOK2 from '../assets/gallery/byok2.webp';
 import BYOK3 from '../assets/gallery/byok3.webp';
+import WBS1 from '../assets/gallery/WBS1.jpeg';
+import WBS2 from '../assets/gallery/WBS2.jpeg';
+import WBS3 from '../assets/gallery/WBS3.jpeg';
+import WBS4 from '../assets/gallery/WBS4.jpeg';
+import WBS5 from '../assets/gallery/WBS5.jpeg';
+import WBS6 from '../assets/gallery/WBS6.jpeg';
+import WBS7 from '../assets/gallery/WBS7.HEIC';
+import WBS8 from '../assets/gallery/WBS8.HEIC';
+
 
 const gallery = [
+	{
+		id: 0,
+		show: true,
+		name: 'Welcome Back Social Spring 2022',
+		link: 'welcome-back-social-spring-2022',
+		image: WelcomeBackSocial, 
+		location: 'Houston, TX',
+		date: '01/27/2021',
+		pictures: [WBS1, WBS2, WBS3, WBS4, WBS5, WBS6, WBS7, WBS8]
+	},
 	{
 		id: 0,
 		show: true,
