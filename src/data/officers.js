b2dd8dbfd2e287ca0acc0f23606872c6e090cbef
LeftAutomated <bryantle2021@gmail.com>
2022-08-26 19:57:10 -0500

    update officers for fall 2022

--- src/data/officers.js
+++ src/data/officers.js
@@ -1,23 +1,201 @@
 import Dr_Yun from '../assets/officers/Dr_Yun.webp';
-import Nykolas from '../assets/officers/Nykolas.webp';
-import Zuha from '../assets/officers/Zuha.webp';
-import Kevin from '../assets/officers/KevinF.webp';
-import Ahmed from '../assets/officers/AhmedQ.webp';
-import Christian from '../assets/officers/Christian.webp';
-import Erick from '../assets/officers/ErickP.webp';
-import Matthew from '../assets/officers/MattP.webp';
-import Ali from '../assets/officers/Ali.webp';
-import Chetoo from '../assets/officers/ChetOO.webp';
-import Keenan from '../assets/officers/Keenan2.webp';
-import Isaiah from '../assets/officers/Isiah.webp';
-import Manzana from '../assets/officers/Manasa.webp';
-import David from '../assets/officers/David.webp';
-import Anil from '../assets/officers/AnilS.webp';
-import Nadiia from '../assets/officers/Nadiia.webp';
-import Mihir from '../assets/officers/Mihir.webp';
-import MatthewT from '../assets/officers/MatthewT.webp';
+import Placeholder from '../assets/officers/Placeholder.webp';
 
-const officers = [
+import Vanessa from '../assets/officers/fall2022/Vanessa.webp';
+import Frank from '../assets/officers/fall2022/Frank.webp';
+import David2 from '../assets/officers/fall2022/David.webp';
+import Haley from '../assets/officers/fall2022/Haley.webp';
+import Isaiah2 from '../assets/officers/fall2022/Isaiah.webp';
+import Mihir2 from '../assets/officers/fall2022/Mihir.webp';
+import Nathan from '../assets/officers/fall2022/Nathan.webp';
+import Nitin from '../assets/officers/fall2022/Nitin.webp';
+import Sana from '../assets/officers/fall2022/Sana.webp';
+import Thejal from '../assets/officers/fall2022/Thejal.webp';
+import Aman from '../assets/officers/fall2022/Aman.webp';
+import Bryant from '../assets/officers/fall2022/Bryant.webp';
+
+import Nykolas from '../assets/officers/spring2022/Nykolas.webp';
+import Zuha from '../assets/officers/spring2022/Zuha.webp';
+import Kevin from '../assets/officers/spring2022/KevinF.webp';
+import Ahmed from '../assets/officers/spring2022/AhmedQ.webp';
+import Christian from '../assets/officers/spring2022/Christian.webp';
+import Erick from '../assets/officers/spring2022/ErickP.webp';
+import Matthew from '../assets/officers/spring2022/MattP.webp';
+import Ali from '../assets/officers/spring2022/Ali.webp';
+import Chetoo from '../assets/officers/spring2022/ChetOO.webp';
+import Keenan from '../assets/officers/spring2022/Keenan2.webp';
+import Isaiah from '../assets/officers/spring2022/Isiah.webp';
+import Manzana from '../assets/officers/spring2022/Manasa.webp';
+import David from '../assets/officers/spring2022/David.webp';
+import Anil from '../assets/officers/spring2022/AnilS.webp';
+import Nadiia from '../assets/officers/spring2022/Nadiia.webp';
+import Mihir from '../assets/officers/spring2022/Mihir.webp';
+import MatthewT from '../assets/officers/spring2022/MatthewT.webp';
+
+export const officersFall2022 = [
+	{
+		id: 0,
+		name: 'Dr.Chang Yun',
+		image: Dr_Yun,
+		linkedin: 'https://www.linkedin.com/in/chang-yun-94340a14/',
+		email: 'phys94@cougarcs.com',
+		title: 'Faculty Advisor',
+		open: false,
+		show: false,
+	},
+	{
+		id: 1,
+		name: 'Vanessa Zambrano',
+		image: Vanessa,
+		linkedin: 'https://www.linkedin.com/in/vlzambr/',
+		email: 'president@cougarcs.com',
+		title: 'President',
+		open: false,
+		show: true,
+	},
+	{
+		id: 2,
+		name: 'Christian Ayala',
+		image: Christian,
+		linkedin: 'https://www.linkedin.com/in/christian-ayala--/',
+		email: 'vp.internal@cougarcs.com',
+		title: 'Vice President Internal Affairs',
+		open: false,
+		show: true,
+	},
+	{
+		id: 3,
+		name: 'Nathan Nguyen',
+		image: Nathan,
+		linkedin: 'https://www.linkedin.com/in/nathvnguyen/',
+		email: 'vp.external@cougarcs.com',
+		title: 'Vice President External Affairs',
+		open: false,
+		show: true,
+	},
+	{
+		id: 4,
+		name: 'Anil Shanker',
+		image: Anil,
+		linkedin: 'https://www.linkedin.com/in/anil-shanker/',
+		email: 'activities@cougarcs.com',
+		title: 'Vice President Activities',
+		open: false,
+		show: true,
+	},
+	{
+		id: 5,
+		name: 'Ali Zain Charolia',
+		image: Ali,
+		linkedin: 'https://www.linkedin.com/in/alizaincharolia/',
+		email: 'alizain@cougarcs.com',
+		title: 'Vice President CodeRED',
+		open: false,
+		show: true,
+	},
+	{
+		id: 6,
+		name: 'Haley Heyman',
+		image: Haley,
+		linkedin: 'https://www.linkedin.com/in/haley-heyman/',
+		email: 'communications@cougarcs.com',
+		title: 'Director of Communications',
+		open: false,
+		show: true,
+	},
+	{
+		id: 7,
+		name: 'Isaiah Johnson',
+		image: Isaiah2,
+		linkedin: 'https://www.linkedin.com/in/the-isaiah-johnson/',
+		email: 'designer@cougarcs.com',
+		title: 'Designer',
+		open: false,
+		show: true,
+	},
+	{
+		id: 8,
+		name: 'David Wu',
+		image: David2,
+		linkedin: 'https://www.linkedin.com/in/david-wu1/',
+		email: 'marketing@cougarcs.com',
+		title: 'Director of Marketing',
+		open: false,
+		show: true,
+	},
+	{
+		id: 9,
+		name: 'Sana Akbani',
+		image: Sana,
+		linkedin: 'https://www.linkedin.com/in/sana-akbani/',
+		email: 'workshops@cougarcs.com',
+		title: 'Director of Workshops',
+		open: false,
+		show: true,
+	},
+	{
+		id: 10,
+		name: 'Aman Surani',
+		image: Aman,
+		linkedin: 'https://www.linkedin.com/in/amansurani/',
+		email: 'corporate.relations@cougarcs.com',
+		title: 'Director of Corporate Relations',
+		open: false,
+		show: true,
+	},
+	{
+		id: 11,
+		name: 'Thejal Harrichand',
+		image: Thejal,
+		linkedin: 'https://www.linkedin.com/',
+		email: 'socials@cougarcs.com',
+		title: 'Director of Socials',
+		open: false,
+		show: true,
+	},
+	{
+		id: 12,
+		name: 'Nitin Valisetty',
+		image: Nitin,
+		linkedin: 'https://www.linkedin.com/in/nitin-valisetty/',
+		email: 'operations@cougarcs.com',
+		title: 'Director of Operations',
+		open: false,
+		show: true,
+	},
+	{
+		id: 13,
+		name: 'Frank Bui',
+		image: Frank,
+		linkedin: 'https://www.linkedin.com/in/frank-bui/',
+		email: 'tutoring@cougarcs.com',
+		title: 'Director of Tutoring',
+		open: false,
+		show: true,
+	},
+	{
+		id: 14,
+		name: 'Bryant Le',
+		image: Bryant,
+		linkedin: 'https://www.linkedin.com/in/bnle/',
+		email: 'webmaster@cougarcs.com',
+		title: 'Web Master',
+		open: false,
+		show: true,
+	},
+	{
+		id: 15,
+		name: 'Mihir Sahu',
+		image: Mihir2,
+		linkedin: 'https://www.linkedin.com/in/the-mihir-sahu/',
+		email: 'infosec@cougarcs.com',
+		title: 'Director of Information Security',
+		open: false,
+		show: true,
+	},
+];
+
+export const officersSpring2022 = [
 	{
 		id: 0,
 		name: 'Dr.Chang Yun',
@@ -199,5 +377,3 @@ const officers = [
 		show: true,
 	},
 ];
-
-export default officers;
